f81c8b8657193470fd9dea72fcdc52a1c45ef099
Juan Antonio Pavon <jpavon@ohkasystems.dom>
2016-08-06 14:22:39 -0500

    add target

---
+++ ACharts/ACharts.h
@@ -0,0 +1,19 @@
+//
+//  ACharts.h
+//  ACharts
+//
+//  Created by Juan Antonio Pavon on 06/08/16.
+//  Copyright © 2016 dcg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ACharts.
+FOUNDATION_EXPORT double AChartsVersionNumber;
+
+//! Project version string for ACharts.
+FOUNDATION_EXPORT const unsigned char AChartsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ACharts/PublicHeader.h>
+
+
